561efe9aa3e6257d5d61d10e1895a4574468954e
surya16-me <suryaadjie08@gmail.com>
2023-04-12 08:22:48 +0700

    sc quiz

---
+++ src/penilaianlomba/PenilaianLomba.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package penilaianlomba;
+
+import penilaianlomba.view.KategoriLomba;
+
+/**
+ *
+ * @author 5urya
+ */
+public class PenilaianLomba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KategoriLomba k = new KategoriLomba();
+        k.setLocationRelativeTo(null);
+        k.setVisible(true);
+    }
+    
+}
